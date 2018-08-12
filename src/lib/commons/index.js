07547733a64f53a66b52d8bfa6ce9bdd50daeba5
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-12 16:29:39 +0300

    Add getSubMatrix and getSector functions in lib/commons/utils.js

--- src/lib/commons/index.js
+++ src/lib/commons/index.js
@@ -1,2 +1,2 @@
-import { getColumn } from './utils';
-export { getColumn };
+import { getColumn, getSector } from './utils';
+export { getColumn, getSector };
--- src/lib/commons/utils.js
+++ src/lib/commons/utils.js
@@ -7,6 +7,40 @@ const getColumn = (board,columnIndex) => (
   []
 ));
 
+const getSubMatrix =  (board,topX,topY,bottomX,bottomY) => (
+  board.reduce( (acc,row,rowIndex) => (
+    (rowIndex>=topX && rowIndex<=bottomX) ?
+      [...acc,
+        row.filter( (elem,columnIndex) =>
+        (columnIndex>=topY && columnIndex<=bottomY))
+      ]
+      :
+      acc
+    ),
+    []
+));
+
+const getSector = (board,x,y) => {
+  const horizontalSector = Math.floor(x/3);
+  const verticalSector = Math.floor(y/3);
+
+  const topX = verticalSector*3;
+  const topY = horizontalSector*3;
+
+  const subBoard = getSubMatrix(board,topX,topY,topX+2,topY+2);
+
+  return subBoard.reduce( (acc,row) => ([...acc,...row]), []);
+
+};
+
+
+export { getColumn, getSector };
+
+
+// ------DOCUMENTATION------
+
+
+// --getColumn--
 // returns the column in the 'columnIndex' index as an array.
 // board should be in the format:
 // [
@@ -15,4 +49,11 @@ const getColumn = (board,columnIndex) => (
 //   [8,22]
 // ]
 
-export { getColumn };
+
+// --getSubMatrix--
+// returns the 2D subarray enclosed by the top left and bottom right coordinates specified
+
+//
+// --getSector--
+// takes as params a sudoku board and coordinates of an element
+// returns the sudoku-sector the element belongs to, in a single 1D array
