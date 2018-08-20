10cf0e44d58d92a9ec486b65343f403630418606
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-20 20:53:52 +0300

    Add isBoardValid, preSelected and isCellValid in app's state. Changed reducer to update these values

--- src/reducers/sudokuReducer.js
+++ src/reducers/sudokuReducer.js
@@ -1,9 +1,11 @@
-import { emptyBoard, isNumberValid, updateCell, solveSudoku } from '../lib';
+import { emptyBoard, updateCell, solveSudoku, cellsValidation, boardValidation } from '../lib';
 
 const initialState = Object.freeze({
   board: emptyBoard(),
+  isCellValid: emptyBoard(true),
+  preSelected: emptyBoard(false),
   isBoardValid: true,
-  sovled: false
+  solved: false
 });
 
 const sudokuReducer = (state = initialState, action = {}) => {
@@ -11,36 +13,54 @@ const sudokuReducer = (state = initialState, action = {}) => {
 
     case 'CHANGE_CELL':
       const {row,column,input} = action.payload;
-
       if(state.solved){
         return state;
       }
 
       if(input===' ' || input==='' || isNaN(input)){
+
+        const newBoard = updateCell(state.board,row,column,0);
+        const newValidationBoard = cellsValidation(newBoard);
+
         return Object.freeze({
           ...state,
-          board: updateCell(state.board,row,column,0)
-        });
-      }
-      
-      else {
-        const validChange = isNumberValid(state.board,row,column,parseInt(input,10));
-        return Object.freeze({
-          ...state,
-          board: updateCell(state.board,row,column,parseInt(input,10)),
-          isBoardValid: state.isBoardValid && validChange
+          board: newBoard,
+          isCellValid: newValidationBoard,
+          preSelected: updateCell(state.preSelected,row,column,false),
+          isBoardValid: boardValidation(newValidationBoard)
         });
       }
 
+      else {
+        const newBoard = updateCell(state.board,row,column,parseInt(input,10));
+        const newValidationBoard = cellsValidation(newBoard);
+
+        return Object.freeze({
+          ...state,
+          board: newBoard,
+          isCellValid: newValidationBoard,
+          preSelected: updateCell(state.preSelected,row,column,true),
+          isBoardValid: boardValidation(newValidationBoard)
+        });
+      }
+
+
     case 'SOLVE_BOARD':
+
       return Object.freeze({
         ...state,
         board: solveSudoku(state.board).board,
         solved: true
       });
 
+
     case 'CLEAR_BOARD':
       return initialState;
+
+
+    case 'SOLVE_INVALID_BOARD':
+      return state;
+
 
     default:
       return state;
