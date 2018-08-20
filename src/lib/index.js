92f34ab4d062d7f20b813468c270fa02691986a0
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-20 20:48:04 +0300

    Add cellsValidation and boardValidation functions

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 import { solveSudoku } from './solveSudoku';
-import { isNumberValid } from './validationCheck';
+import { isNumberValid, cellsValidation, boardValidation } from './validationCheck';
 import { updateCell, emptyBoard } from './commons';
-export { solveSudoku, isNumberValid, updateCell, emptyBoard };
+export { solveSudoku, isNumberValid, updateCell, emptyBoard, cellsValidation, boardValidation };
--- src/lib/validationCheck.js
+++ src/lib/validationCheck.js
@@ -1,4 +1,4 @@
-import { getColumn, getSector } from './commons';
+import { getColumn, getSector, updateCell } from './commons';
 
 const availableNumbers = [1,2,3,4,5,6,7,8,9];
 
@@ -31,4 +31,26 @@ const isNumberValid = (board, i, j, num) => (
   && isSectorValid(board, i, j, num)
 );
 
-export { isNumberValid };
+const cellsValidation = (board) => (
+// takes a sudoku board and returns a 9x9 boolean matrix
+// with (i,j) === true iff the number in position (i,j) is valid
+// according to sudoku rules
+
+  board.map( (row,rowIndex) => (
+    row.map( (elem,columnIndex) => (
+      elem===0 ||
+      isNumberValid(
+        updateCell(board,rowIndex,columnIndex,0),
+        rowIndex,
+        columnIndex,
+        elem
+      ))
+   )
+ ))
+);
+
+const boardValidation = (cellsValid) => {
+  return cellsValid.every( row => row.every( x => x));
+}
+
+export { isNumberValid, cellsValidation, boardValidation };
