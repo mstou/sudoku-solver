3cd89c8cee1c07d95f43ab331c191a8007321856
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-20 03:43:14 +0300

    Add class for sudoku buttons

--- src/components/buttons/sudokuButtons.js
+++ src/components/buttons/sudokuButtons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Buttons = ({onSolveClick, onClearClick}) => (
-  <div>
+  <div className="sudoku-buttons">
     <button className="SolveButton" onClick={onSolveClick}> Solve </button>
     <button className="ClearButton" onClick={onClearClick}> Clear </button>
   </div>
