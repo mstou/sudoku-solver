bc3d27c8de0f74b4ccc005a8317608c33ef2faab
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-20 03:58:32 +0300

    Add a function to compute cell's class based on grid location

--- src/components/board/row/cell/cell.js
+++ src/components/board/row/cell/cell.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+const computeClassName = (row,column) => {
+  let className = '';
+  if( column===2 || column===5 ){
+    className += 'borderRight';
+  }
+  if( row===2 || row===5 ){
+    if(className!==''){
+      className +=' ';
+    }
+    className += 'borderBottom';
+  }
+  return className;
+}
+
 const Cell = ({data, row, column, onChange}) => (
-  <div className="sudoku-cell">
+  <div className={`sudoku-cell ${computeClassName(row,column)}`}>
     <input
       value={(data!==0) ? data : '' }
       onChange={(e) => onChange(row,column,e.target.value.slice(-1))}
