b46c52e84c046f3260a7eb8d25af1758306910e3
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-20 03:42:28 +0300

    Enclosed cell in a div container

--- src/components/board/row/cell/cell.js
+++ src/components/board/row/cell/cell.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './styles.css';
 
 const Cell = ({data, row, column, onChange}) => (
-  <input
-    className="sudoku-cell"
-    value={(data!==0) ? data : '' }
-    onChange={(e) => onChange(row,column,e.target.value.slice(-1))}
-  />
+  <div className="sudoku-cell">
+    <input
+      value={(data!==0) ? data : '' }
+      onChange={(e) => onChange(row,column,e.target.value.slice(-1))}
+    />
+  </div>
 );
 
 export default Cell;
