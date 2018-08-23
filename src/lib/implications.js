14ec6a30a84fc78eccee8689b774607c0cf85509
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-23 17:19:15 +0300

    Add makeImplications function

--- src/lib/implications.js
+++ src/lib/implications.js
@@ -1,4 +1,4 @@
-import { getColumn, getSector } from './commons';
+import { getColumn, getSector, updateCell } from './commons';
 
 const availableNumbers = [1,2,3,4,5,6,7,8,9];
 
@@ -39,4 +39,49 @@ const eliminateSectorNumbers = (board, possibleNumbers) => (
   ))
 );
 
-export { attachPossibleNumbers, eliminateRowNumbers, eliminateColumnNumbers, eliminateSectorNumbers };
+const findImplications1D = (possibleNumbersRow,rowIndex) => (
+  possibleNumbersRow.reduce( (acc,elem,elemIndex) => (
+    (elem.length===1) ?
+    [
+      ...acc,
+      {
+      row: rowIndex,
+      column: elemIndex,
+      num: elem[0]
+    }]
+    :
+    acc
+  ),[])
+);
+
+const makeImplications = (board) => {
+  const possibleNumbers = eliminateRowNumbers(
+    board,
+    eliminateColumnNumbers(
+      board,
+      eliminateSectorNumbers(
+        board,
+        attachPossibleNumbers(board)
+      )
+    ));
+
+  const implications = possibleNumbers.reduce( (acc,row,rowIndex) => (
+    [...acc, ...findImplications1D(row,rowIndex)]
+  ), []);
+
+  return (implications.length===0) ? board
+  :
+  makeImplications(
+    implications.reduce( (acc,implication) => (
+    updateCell(acc,implication.row,implication.column,implication.num)
+  ), [...board ] )
+ );
+
+}
+
+//makeImplications function, accepts a sudokuBoard, and checks if the current state of the board implies the position
+// of some numbers. If so it fills them, and iterates until there are no such implications
+
+
+
+export { makeImplications };
