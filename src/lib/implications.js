46c726f09307b11699a2b19d3771ad8b292b04e7
Miltiadis Stouras <m.stouras@gmail.com>
2018-08-23 15:55:47 +0300

    Change returned array in attachPossibleNumbers, implement eliminateRowNumbers

--- src/lib/implications.js
+++ src/lib/implications.js
@@ -1,16 +1,24 @@
+import { getColumn, getSector } from './commons';
 
 const availableNumbers = [1,2,3,4,5,6,7,8,9];
 
 const attachPossibleNumbers = (board) => (
   // takes a sudoku board and returns a 9x9 matrix with each
-  // cell being an object that contains the number on the board and an
-  // array with all the available numbers to fill the cell with.
+  // cell containing an array with all the available numbers to fill the cell with.
   board.map( row => (
-    row.map( cell => ({
-      data: cell,
-      possibleNumbers: (cell===0) ? availableNumbers : []
-    }))
+    row.map( cell => (
+      (cell===0) ? availableNumbers : []
+    ))
   ))
 );
 
-export { attachPossibleNumbers };
+const eliminateRowNumbers = (board, possibleNumbers) => (
+  // for each cell's possible numbers, it eliminates nums that appear elsewhere in the same row
+  possibleNumbers.map( (row,rowIndex) => (
+    row.map( cellNums =>
+      cellNums.filter( num => !board[rowIndex].includes(num) )
+    )
+  ))
+);
+
+export { attachPossibleNumbers, eliminateRowNumbers };
